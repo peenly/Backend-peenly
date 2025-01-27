2cf7f7c4cae6b058a940a17a8510e38977048bd0
Reyinbee <wealthbassey99@gmail.com>
2025-01-27 13:59:58 +0100

    main

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors({origin: "http://localhost:3000"}))
 
 
 app.get('/', (req, res)=>{
