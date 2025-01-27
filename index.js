ed0e1a35229cb5c4476b073419e12feb3eba0442
Reyinbee <wealthbassey99@gmail.com>
2025-01-27 14:42:16 +0100

    main

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors({origin: "http://localhost:3000"}))
 
 
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow localhost:3000 only
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If your requests include credentials like cookies
+};
+
+app.use(cors(corsOptions));
+
+
 app.get('/', (req, res)=>{
     res.send("Welcome to Backend")
 })
